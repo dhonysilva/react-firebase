d86d72242a97ea70b3141ea64d62415916f866c8
Dhony <dhony.web@gmail.com>
2020-12-06 16:15:35 -0300

    Learn how to use React and Firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Flex, Spinner, VStack } from "@chakra-ui/core";
+import React, { useEffect, useState } from 'react';
+import Navbar from "./components/navbar";
+import Post from "./components/post";
+import db from "./lib/firebase";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [posts, setPosts] = useState([]);
+    const [isLoading, setLoading] = useState(true);
+
+    useEffect(() => {
+        db.collection("posts")
+            .orderBy("createdAt", "desc")
+            .get()
+            .then((querySnapshot) => {
+                const data = querySnapshot.docs.map((doc) => ({
+                    id: doc.id,
+                    ...doc.data(),
+                }));
+
+                setPosts(data);
+                setLoading(false);
+            });
+    }, []);
+
+    useEffect(() => {
+
+        db.collection("posts")
+            .orderBy("createdAt", "desc")
+            .onSnapshot((querySnapshot) => {
+                const _posts = [];
+
+                querySnapshot.forEach((doc) => {
+                    _posts.push({
+                        id: doc.id,
+                        ...doc.data(),
+                    });
+                });
+                setPosts(_posts);
+            });
+    }, []);
+
+    if (isLoading) {
+        return (
+            <Flex minH="100vh" justifyContent="center" alignItems="center">
+                <Spinner />
+            </Flex>
+        );
+    }
+
+    return (
+        <>
+        <Navbar />
+        <Container maxW="md" centerContent p={8}>
+            <VStack spacing={8} w="100%">
+                {posts.map((post) => (
+                    <Post post={post} key={post.id} />
+                ))}
+            </VStack>
+        </Container>
+        </>
+    );
+};
 
 export default App;
